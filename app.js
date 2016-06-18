e235165dfab8ad333048da9645e2acf638037fcd
chrispaynes <cdpaynes@gmail.com>
2016-06-17 23:00:53 -0500

    Adds logging function, implements more ES2015 and updates comments

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ const express = require("express"),
       livereload = require("express-livereload"),
       helpers = require('express-helpers')(app);
 
+function logger(req, res, next){
+  console.log(new Date(), req.method, req.url);
+  next();
+}
+
 // app.set modifies expressJS application settings
 // sets a variable for an expressJS app
 app.set("view engine", "ejs");
 
 // Mount the middleware at "/static" to serve static content only when their request path is prefixed with "/static".
 
-// GET /style.css etc
-app.use(express.static(__dirname + '/public'));
 
-// app.locals properties persist throughout an application's life
-// makes data available across all routes and templates
+/*
+app.use([path,] function [, function...])
+mounts middleware functions at the specified path.
+mounting static image and css files from the "public" directory
+mounting logger() to displays log information to the console
+*/
+app.use(express.static(__dirname + '/public'),
+        logger
+);
 
-
+/*
+app.locals{} properties specify variables that persist throughout the app
+app.locals makes data available across all app routes and templates
+*/
 app.locals = {
   pagetitle: "node_express_app",
   app_routes: ["home", "about", "contact"],
@@ -30,19 +43,19 @@ app.locals = {
   //     res.send(name + " was here");
   // });
 
+// app.get(path, callback [, callback ...])
 // specifies actions when a user requests a route
 app.get(["/", "/index", "/home"], routes.index);
 app.get("/about", routes.about);
 app.get("/contact", routes.contact);
 
-// returns bad routes for pattern matching on nonexistant routes
-app.get("*", function(req, res) {
-    res.send("Bad Route");
-});
 
-const server = app.listen(4000, function(){
-    console.log("Listening on http://localhost:4000"); 
-});
+// sends text error to the browser when a user requests a nonexistant route
+app.get("*", (req, res) => res.status(404).send("Bad Route"));
+
+const server = app.listen(4000, () => 
+    console.log("Listening on http://localhost:4000") 
+);
 
 livereload(app, config={
   watchDir: __dirname + "/"
