c5e07be1afaa221fa41c3ca74dc645243769bc17
chrispaynes <cdpaynes@gmail.com>
2016-06-18 00:13:53 -0500

    Implements Morgan and updates javascript syntax to ES2015

--- app.js
+++ app.js
@@ -2,28 +2,22 @@ const express = require("express"),
       app = express(),
       routes = require("./routes"),
       livereload = require("express-livereload"),
-      helpers = require('express-helpers')(app);
-
-function logger(req, res, next){
-  console.log(new Date(), req.method, req.url);
-  next();
-}
+      helpers = require("express-helpers")(app),
+      morgan = require("morgan");
 
 // app.set modifies expressJS application settings
 // sets a variable for an expressJS app
 app.set("view engine", "ejs");
 
-// Mount the middleware at "/static" to serve static content only when their request path is prefixed with "/static".
-
-
 /*
 app.use([path,] function [, function...])
-mounts middleware functions at the specified path.
+mounts middleware functions at a specified path.
+each server request will run through these functions
 mounting static image and css files from the "public" directory
 mounting logger() to displays log information to the console
 */
 app.use(express.static(__dirname + '/public'),
-        logger
+        morgan("common", {date: "web"})
 );
 
 /*
@@ -49,13 +43,12 @@ app.get(["/", "/index", "/home"], routes.index);
 app.get("/about", routes.about);
 app.get("/contact", routes.contact);
 
-
 // sends text error to the browser when a user requests a nonexistant route
 app.get("*", (req, res) => res.status(404).send("Bad Route"));
 
-const server = app.listen(4000, () => 
-    console.log("Listening on http://localhost:4000") 
-);
+const server = app.listen(4000, (err, res) =>
+  err => console.log("Server error"),
+  res => console.log("Listening on http://localhost:4000"));
 
 livereload(app, config={
   watchDir: __dirname + "/"
