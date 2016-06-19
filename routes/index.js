861e2528ea7d8d5756700e57e9adcb33480405b4
chrispaynes <cdpaynes@gmail.com>
2016-06-18 21:16:25 -0500

    Corrects rendered views and removes index users

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-// gets application root route (index.html)
-// renders the default.ejs template and sends a HTML string to the client
-// by default the app expects templates to be in the /views/ folder
-// pass in data to a template within the "locals" parameter
-// res.render(view [, locals] [, callback])
+/*
+gets application root route (index.html)
+renders the default.ejs template and sends a HTML string to the client
+by default the app expects templates to be in the /views/ folder
+pass in data to a template within the "locals" parameter
+res.render(view [, locals] [, callback])
+*/
 exports.index = function(req, res) {
     res.render("index", {
       title: "Index",
-      classname: "home",
-      users: ["Tom", "Dick", "Harry"],
+      classname: "home"
     });
 
     // to specify a template folder other than the default /views/ folder...
@@ -16,14 +17,14 @@ exports.index = function(req, res) {
 };
 
 exports.about = function(req, res) {
-    res.render("index", {
+    res.render("about", {
       title: "About",
       classname: "about"
     });
 };
 
 exports.contact = function(req, res) {
-    res.render("index", {
+    res.render("contact", {
       title: "Contact",
       classname: "contact"
     });
