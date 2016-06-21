d6b99b449a3c0a8000bc4fb3d1b9ac0ddcef2bc9
chrispaynes <cdpaynes@gmail.com>
2016-06-20 21:54:16 -0500

    Adds 'title' property to res.rend()

--- errors/errors.js
+++ errors/errors.js
@@ -1,8 +1,14 @@
 exports.code404 = (req, res) => {
-  res.type("text/html").status(404).render("404");
+  res.type("text/html").status(404)
+  res.render("404", {
+    title: "404 : Not Found"
+  });
 };
 
 exports.code500 = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).render("500");
+  res.status(500)
+  res.render("500", {
+    title: "500: Internal Server Error"
+  });
 };
